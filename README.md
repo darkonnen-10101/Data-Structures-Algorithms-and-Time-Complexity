f5120cc03b151dfa07a20133852b7b0bbb821a59
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-02-09 00:26:55 -0300

    gotta solve them all

---
+++ README.md
@@ -0,0 +1,3 @@
+# I want to be the very best, like no one ever was!
+
+Cracking the coding interview excercises. Udemy course
---
+++ src/com/darkonnen/excercises/timecomplexity/TypesOfBigO.java
@@ -0,0 +1,20 @@
+package com.darkonnen.excercises.timecomplexity;
+
+public class TypesOfBigO {
+	
+//	Commons types of Big O
+//	
+//	O(n!) - Factorial
+//	O(2^n) - Exponential
+//	O(n^2) - Quadratic
+//	O(n log n) - Linearithmic
+//	O(n) - Linear
+//	O(log n) - Logarithmic
+//	O(1)
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
