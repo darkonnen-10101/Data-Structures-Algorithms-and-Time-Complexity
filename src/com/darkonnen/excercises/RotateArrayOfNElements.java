3029421a8b903caf2f7c1284220d12c05a77da11
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2021-02-10 00:14:16 -0300

    wtf this excercise

--- src/com/darkonnen/excercises/RotateArrayOfNElements.java
+++ src/com/darkonnen/excercises/RotateArrayOfNElements.java
@@ -17,14 +17,60 @@ public class RotateArrayOfNElements {
 
 		return result;
 	}
+	
+	public int[] rotate(int[] input, int rotateBy, int size) {
+		
+		int index;
+		int savedIndex;
+		int rotateByIndex;
+		int temporary;
+		
+		rotateBy = rotateBy % size;
+		
+		int greatestCommonDivisor = findGreatestCommonDivisor(rotateBy, size);
+		
+		for(int i = 0; i < greatestCommonDivisor; i++) {
+			temporary = input[i];
+			savedIndex = i;
+			
+			while(true) {
+				rotateByIndex = savedIndex + rotateBy;
+				
+				if(rotateByIndex >= size) {
+					rotateByIndex = rotateByIndex - size;
+				}
+				
+				if(rotateByIndex == i) {
+					break;
+				}
+				
+				input[savedIndex] = input[rotateByIndex];
+				savedIndex = rotateByIndex;
+				input[savedIndex] =  temporary;
+
+			}
+		}
+		
+		return input;
+	}
+
+	private int findGreatestCommonDivisor(int rotateBy, int size) {
+		
+		if(size == 0) {
+			return rotateBy;
+		} else {
+			return findGreatestCommonDivisor(size, rotateBy % size);
+		}
+	}
 
 	public static void main(String[] args) {
 		RotateArrayOfNElements ex = new RotateArrayOfNElements();
 
 		int[] array = {1,2,3,4,5,6,7};
 		
-		System.out.println(Arrays.toString(ex.rotate(array, 2)));
-		
+//		System.out.println(Arrays.toString(ex.rotate(array, 2)));
+		System.out.println(Arrays.toString(ex.rotate(array, 2, 7)));
+
 	}
 
 }
